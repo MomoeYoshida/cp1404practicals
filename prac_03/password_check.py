5d8b0cd38d250d88dd2c929878defdd8c189f02a
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-09 12:00:25 +0900

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,4 +1,5 @@
 """
+Modify the program below
 Write a program that asks the user for a password, with
 error-checking to repeat if the password doesn't meet a
 minimum length set by a variable. The program should then
@@ -8,10 +9,24 @@ print asterisks (*) as long as the word.
 MINIMUM_LENGTH = 6
 
 
-def get_password():
-    """Get a password of valid length and print asterisks."""
-    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-    while len(password) < MINIMUM_LENGTH:
-        password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+def main():
+    """Get and print password using functions."""
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
 
-    print('*' * len(password))
+
+def get_password(minimum_length):
+    """Get password (ensuring it meets the minimum_length requirement)."""
+    password = input("Enter password of at least {} characters: ".format(minimum_length))
+    while len(password) < minimum_length:
+        print("Error - at least {} characters".format(minimum_length))
+        password = input("Enter password of at least {} characters: ".format(minimum_length))
+    return password
+
+
+def print_asterisks(sequence):
+    """Print asterisks (*) as long as the characters."""
+    print('*' * len(sequence))
+
+
+main()
