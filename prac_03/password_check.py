a0518c5fef21741f4f288ee53e0fec34d6293e2d
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-11 10:31:47 +0900

    Edit the title

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,5 +1,5 @@
 """
-Modify the program below
+Modify the program below to use functions.
 Write a program that asks the user for a password, with
 error-checking to repeat if the password doesn't meet a
 minimum length set by a variable. The program should then
