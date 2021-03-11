98dd57d79556bd8e8c439913494f567627b735a6
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-11 11:49:55 +0900

    Add answer to part.c

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,20 +2,11 @@
 Refactor the broken program determining score status in prac_01 to use functions
 """
 
-import random
-
 
 def main():
     """Get a score and print its status."""
     score = float(input("Enter score: "))
-    # score = generate_random_score()
-    # print("Random score: {}".format(score))
     print(determine_status(score))
-
-
-# def generate_random_score():
-#     """Generate a random score."""
-#     return int(random.randint(0, 100))
 
 
 def determine_status(score):
