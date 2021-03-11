902661e030283777356e2faff278b5d53a6fe6d2
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-11 11:42:29 +0900

    Add answer to part.c

--- prac_03/ascii_table.py
+++ prac_03/ascii_table.py
@@ -5,47 +5,25 @@ ASCII table
 LOWER = 33
 UPPER = 127
 
-char = input("Enter a character: ")
-print("The ASCII code for {} is {}".format(char, ord(char)))  # ord() to convert characters to ASCII integer values
-number = int(input("Enter a number between {} and {}: ".format(LOWER, UPPER)))
-while not LOWER <= number <= UPPER:
-    number = int(input("Enter a number between {} and {}: ".format(LOWER, UPPER)))
-print("The character for {} is {}".format(number, chr(number))) # chr() to convert ASCII integer values to characters
 
-# 3. Add on to this program by writing code that displays a table with two columns, one for the numeric ASCII value and
-# the other for the character itself. Use the string format() method to align the text nicely in two columns.
-# Print the values between LOWER and UPPER.
-for value in range(LOWER, UPPER + 1):
-    print("{:3} {:>3}".format(value, chr(value)))
+def main():
+    """Get and convert ASCII values/characters."""
+    char = input("Enter a character: ")
+    print("The ASCII code for {} is {}".format(char, ord(char)))  # ord() to convert characters to ASCII integer values
+    number = get_number_between(LOWER, UPPER)
+    print("The character for {} is {}".format(number, chr(number)))
 
-"""
-Extension
-"""
-# ASCII Columns Challenge
-# Ask the user for how many columns to print.
-MAX_COLUMNS = 8
-MIN_COLUMNS = 2
+    # ASCII table (no columns)
+    for value in range(LOWER, UPPER + 1):
+        print("{:3} {:>4}".format(value, chr(value)))
 
-columns = int(input("Enter number of columns: "))
-while not MIN_COLUMNS <= columns <= MAX_COLUMNS:
-    print("Please use a value between {} and {}".format(MIN_COLUMNS, MAX_COLUMNS))
-    columns = int(input("Enter number of columns: "))
 
-number_of_values = UPPER - LOWER + 1
-rows = number_of_values // columns  # floor division (//) is a normal division operation
-# except that it returns the largest possible integer, which is <= the normal division result.
+def get_number_between(lower=LOWER, upper=UPPER):
+    """Get a valid number between the given parameters."""
+    number = int(input("Enter a number between {} and {}: ".format(lower, upper)))
+    while not lower <= number <= upper:
+        number = int(input("Enter a number between {} and {}: ".format(lower, upper)))
+    return number
 
-print("Version 1: Horizontal then vertical ordering")
-value = LOWER
-for row in range(rows):
-    for column in range(columns):
-        print("{:6} {:>2}".format(value, chr(value)), end="")
-        value += 1
-    print()
 
-# last row is special as it may not have all columns
-# start where we left off and only print up to UPPER
-starting_value = value
-for value in range(starting_value, UPPER + 1):
-    print("{:6} {:>2}".format(value, chr(value)), end="")
-print("\n")
+main()
