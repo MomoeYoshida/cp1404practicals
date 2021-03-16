39cd8cc96cd806e40f2bf3a819f7f270840c3f31
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-16 21:01:37 +0900

    Complete the TODO instructions in the code

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -16,24 +16,39 @@ print(first_initials)
 first_initials = [name[0] for name in names]
 print(first_initials)
 
-# list comprehension that creates a list containing the initials
-# splits each name and adds the first letters of each part to a string
+# for loop that creates a list containing the initials splits each name and adds
+# the first letters of each part of a string
+full_initials = []
+for name in full_names:
+    full_initials.append(name.split()[0][0] + name.split()[1][0])
+print(full_initials)
+
+# list comprehension that does the same thing as the loop above
 full_initials = [name.split()[0][0] + name.split()[1][0] for name in
                  full_names]
 print(full_initials)
 
 # one more example, using filtering to select only the names that start with A
+a_names = []
+for name in names:
+    if name.startswith('A'):
+        a_names.append(name)
+print(a_names)
+
+# list comprehension that does the same thing as the above
 a_names = [name for name in names if name.startswith('A')]
 print(a_names)
 
-# TODO: use a list comprehension to create a list of all of the full_names
-# in lowercase format
-# lowercase_full_names =
+# list comprehension that creates a list of all of the full_names in lowercase format
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
 
+# list comprehension that creates a list of integers from the above list of strings
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
-# TODO: use a list comprehension to create a list of integers
-# from the above list of strings
-# numbers =
+numbers = [int(almost_number) for almost_number in almost_numbers]
+print(numbers)
 
-# TODO: use a list comprehension to create a list of only the numbers that are
-# greater than 9 from the numbers (not strings) you just created
+# list comprehension that creates a list of only the numbers that are greater than
+# 9 from the numbers (not strings) you just created
+numbers_greater_than_9 = [number for number in numbers if number > 9]
+print(numbers_greater_than_9)
