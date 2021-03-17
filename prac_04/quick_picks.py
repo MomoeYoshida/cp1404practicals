f8f4fa1222e40a2ea7678332e79df3754da1cda1
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-17 12:39:20 +0900

    Add "quick pick" lottery ticket generator program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -16,7 +16,7 @@ def main():
     number_of_quick_picks = int(input("How many quick picks? "))
     while number_of_quick_picks < 0:
         print("Invalid number!")
-        number_of_quick_picks =int(input("How many quick picks? "))
+        number_of_quick_picks = int(input("How many quick picks? "))
 
     for i in range(number_of_quick_picks):
         quick_pick = []
