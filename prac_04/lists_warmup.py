263252e70fd43bcb86fa9cd088f192583275ad78
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-18 17:11:56 +0900

    Write Python expressions to answer the question 1-4

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -4,7 +4,7 @@ Without running the code, write down your answers to
 these questions in your Python file as a comment, then
 use Python to see if you were correct.
 """
-numbers = ["ten", 1, 4, 1, 5, 9, 1]
+numbers = [3, 1, 4, 1, 5, 9, 2]
 
 print(numbers[-1])  # 2
 print(numbers[3])  # 1
@@ -15,8 +15,14 @@ print(7 in numbers)  # False
 print("3" in numbers)  # False
 print(numbers + [6, 5, 3])  # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
-# Get all the elements from numbers except the first two
-print(numbers[2:])
-# Check if 9 is an element of numbers
-print(9 in numbers)
+# Write Python expressions (in the same Python file) to achieve the following
+
+# 1. Change the first element of numbers to "ten"
+numbers[0] = "ten"
+# 2. Change the last element of numbers to 1
+numbers[-1] = 1
+# 3. Get all the elements from numbers except the first two
+print(numbers[2:])  # [4, 1, 5, 9, 1]
+# 4. Check if 9 is an element of numbers
+print(9 in numbers)  # True
 
