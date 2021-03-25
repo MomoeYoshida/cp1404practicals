6a7e41a5c3e88c29ac399bfb439e433af5d8a9b3
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-25 20:38:42 +0900

    Refactor the program to create dictionary of emails-to-names

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,18 +6,30 @@ Use a separate function to get the name from the email. You
 should find the following methods useful: split, join, title.
 Notice the prompt to check if the name is correct: Y/n.
 """
-email_to_name = {}
-email = input("Email: ")
-while email != "":
+
+
+def main():
+    """Store user's emails and names in a dictionary."""
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input("Is your name {}? (Y/n) ".format(name))
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print("{} ({})".format(name, email))
+
+
+def get_name_from_email(email):
+    """Get expected name from email."""
     name = email.split('@')[0]
     name = name.split('.')
     name = " ".join(name).title()
-    confirmation = input("Is your name {}? (Y/n) ".format(name))
-    if confirmation.upper() != "Y" and confirmation != "":
-        name = input("Name: ")
-    email_to_name[email] = name
-    email = input("Email: ")
+    return name
 
-for email, name in email_to_name.items():
-    print("{} ({})".format(name, email))
 
+main()
