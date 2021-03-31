7684a1bdf2c9f213dfef677f0bc82844bcc9c919
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-31 09:46:50 +0900

    Add output as comments

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -8,8 +8,8 @@ def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
     my_car.drive(30)
-    print("fuel =", my_car.fuel)
-    print("odo =", my_car.odometer)
+    print("fuel =", my_car.fuel)  # fuel = 150 (output)
+    print("odo =", my_car.odometer)  # odo = 30 (output)
     print(my_car)
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
