b9a6a63a49312d0a11ae1b594d8762fded6ea7ff
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-02 16:49:35 +0900

    Add a client code to test the Guitar code

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,23 @@
+"""CP1404 Practical - Client code to test the Guitar class."""
+
+from prac_06.guitar import Guitar
+
+CURRENT_YEAR = 2021
+
+
+def run_tests():
+    """Test a Guitar class."""
+    name = "Gibson L-5 CES"
+    year = 1922
+    guitar = Guitar(name, year)
+    another = Guitar("Another Guitar", 2013)
+    # Test that the get_age() method works
+    print("{} get_age() - Expected {}. Got {}".format(guitar.name, 99, guitar.get_age()))
+    print("{} get_age() - Expected {}. Got {}".format(another.name, 8, another.get_age()))
+    # Test that the is_vintage() method works
+    print("{} is_vintage() - Expected {}. Got {}".format(guitar.name, True, guitar.is_vintage()))
+    print("{} is_vintage() - Expected {}. Got {}".format(another.name, False, guitar.is_vintage()))
+
+
+if __name__ == '__main__':
+    run_tests()
