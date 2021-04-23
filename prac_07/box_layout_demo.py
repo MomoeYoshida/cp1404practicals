fa1d3875cc0beede7d3187b9e193fd07b3aaa9b5
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-23 09:25:53 +0900

    Add a new event handler for the clear button

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -10,7 +10,15 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):  # 5. Add a button handler function
         print('test')
-        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text  # 6. Change the text of the label
+        # 9. Since this field has an id, we can now use the information in this text entry field in our button
+        # handler function.
+
+    # 10. Add a new event handler for the clear button
+    def handle_clear(self):
+        """Clear both the text field and the output label."""
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = ""
 
 
 BoxLayoutDemo().run()
--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -3,3 +3,27 @@ A very simple App that has a list of names (strings) and displays each one
 as a separate Label
 """
 
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    """DynamicLabelsApp is a very simple App that has a list of names (strings) and displays each one
+as a separate Label."""
+    def __init__(self):
+        """Construct main app."""
+        self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre", "Momoe Yoshida"]
+
+    def build(self):
+        """"""
+        self.title = "Dynamic Labels"
+        self.display_names()
+
+    def display_names(self):
+        """Display each name as a separate Label."""
+        for name in self.names:
+            temp_label = Label(text=name)
+
+
+DynamicLabelsApp().run()
