f94b869cf97e5269cc391c9c2ef58597328dea56
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-24 11:13:28 +0900

    Add Dynamic Labels Program

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -11,19 +11,23 @@ from kivy.uix.label import Label
 class DynamicLabelsApp(App):
     """DynamicLabelsApp is a very simple App that has a list of names (strings) and displays each one
 as a separate Label."""
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Construct main app."""
+        super().__init__(**kwargs)
         self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre", "Momoe Yoshida"]
 
     def build(self):
-        """"""
+        """Build the Kivy GUI."""
         self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
         self.display_names()
+        return self.root
 
     def display_names(self):
         """Display each name as a separate Label."""
         for name in self.names:
-            temp_label = Label(text=name)
+            temp_label = Label(text=name)  # Create the widgets (labels) in Python code
+            self.root.ids.entries_box.add_widget(temp_label)  # Add these new widgets using the add_widget method
 
 
 DynamicLabelsApp().run()
