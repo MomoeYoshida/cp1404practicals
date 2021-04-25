bec5b5e971cf5a9178fb2e049abe508861895872
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-26 07:11:05 +0900

    Modify codes to use a class variable

--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -7,11 +7,14 @@ from prac_08.car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+    # 1. Add the class variable price_per_km and set it to 1.23
+    price_per_km = 1.23
 
-    def __init__(self, name, fuel, price_per_km):
+    # 2. Change the function so it does not take in the price_per_km.
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)  # Override methods to take account of the characteristics of a Taxi
-        self.price_per_km = price_per_km  # Add new attributes
+        # self.price_per_km = price_per_km  # 2. That's already set by the class variable.
         self.current_fare_distance = 0
 
     def __str__(self):
