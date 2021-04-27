44f36766f8243217a097969fd5579618d84aeb22
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-27 18:49:40 +0900

    Add NumericProperty

--- prac_07/jcu_grade_demo.py
+++ prac_07/jcu_grade_demo.py
@@ -1,8 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import NumericProperty
 
 
 class JCUGradeDemo(App):
+    score = NumericProperty
+
     def build(self):
         self.title = "JCU Grade"
         self.root = Builder.load_file('jcu_grade_layout.kv')
@@ -10,14 +13,14 @@ class JCUGradeDemo(App):
 
     def handle_enter(self):
         print('test')
-        score = float(self.root.ids.input_score.text)
-        if score >= 85:
+        self.score = float(self.root.ids.input_score.text)
+        if self.score >= 85:
             self.root.ids.output_label.text = "HD - Pass with High Distinction"
-        elif score >= 75:
+        elif self.score >= 75:
             self.root.ids.output_label.text = "D - Pass with Distinction"
-        elif score >= 65:
+        elif self.score >= 65:
             self.root.ids.output_label.text = "C - Pass with Credit"
-        elif score >= 50:
+        elif self.score >= 50:
             self.root.ids.output_label.text = "P - Pass"
         else:
             self.root.ids.output_label.text = "F - Fail"
