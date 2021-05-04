175ea58b60ff8054069ae97c42869989369c485e
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-05-04 15:59:58 +0900

    Add a client code to test the UnreliableCar class

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -1,3 +1,20 @@
 """
 CP1404 Practical - Client code to verify each method in the UnreliableCar class.
 """
+from prac_08.unreliable_car import UnreliableCar
+
+
+def main():
+    """Demo test code to test the UnreliableCar class."""
+    high_reliability_car = UnreliableCar("Good Car", 100, 96.9)
+    low_reliability_car = UnreliableCar("Bad Car", 100, 8.5)
+    print("Initial state:", high_reliability_car)
+    print("Initial state:", low_reliability_car)
+    for i in range(1, 15):
+        print("{:8} drove {:2}km.".format(high_reliability_car.name, high_reliability_car.drive(i)))
+        print("{:8} drove {:2}km.".format(low_reliability_car.name, low_reliability_car.drive(i)))
+    print("Final state:", high_reliability_car)
+    print("Final state:", low_reliability_car)
+
+
+main()
