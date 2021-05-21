5a41abc64de87733870fb45d0761e9a55b586276
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-05-22 06:33:15 +0900

    Initial commit

---
+++ prac_10/wiki.py
@@ -0,0 +1,5 @@
+"""
+Write a small script that prompts the user for a page title or search phrase,
+then prints the summary of that page. Use a simple loop that continues doing
+this until the user enters blank input.
+"""
